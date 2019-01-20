97a039e46f9c5f6770cd782ec86b4ac73c5f4cf4
everdimension <everdimension@gmail.com>
2019-01-21 00:00:38 +0300

    Add touch handler (action) to the button
    add debug statement to test the button

--- pitch-hello/ViewController.swift
+++ pitch-hello/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func recordAction(_ sender: Any) {
+        print("button is clicked")
+    }
+    
 }
 
