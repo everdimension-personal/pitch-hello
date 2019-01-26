116745db972f9334532f58ac707d61586fbca090
everdimension <everdimension@gmail.com>
2019-01-26 21:10:58 +0300

    Handle view state updates

--- pitch-hello/ViewController.swift
+++ pitch-hello/ViewController.swift
@@ -14,23 +14,47 @@ class ViewController: UIViewController {
     @IBOutlet weak var recordButton: UIButton!
     @IBOutlet weak var stopRecordingButton: UIButton!
     
+    enum State {
+        case recording
+        case notRecording
+    }
+    
+    var state = State.notRecording
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        print("viewWillAppear")
+        setState(newState: State.notRecording)
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    func setState(newState: State) {
+        state = newState
+        self.update(state: state)
+    }
+    
+    func update(state: State) {
+        switch state {
+        case .recording:
+            recordingLabel.text = "Recording..."
+            recordButton.isEnabled = false
+            stopRecordingButton.isEnabled = true
+        case .notRecording:
+            recordingLabel.text = "Press to Record"
+            recordButton.isEnabled = true
+            stopRecordingButton.isEnabled = false
+        }
+    }
 
     @IBAction func recordAction(_ sender: Any) {
-        print("button is clicked")
-        recordingLabel.text = "Recording..."
+        self.setState(newState: State.recording)
     }
     
     @IBAction func stopRecordAction(_ sender: Any) {
-        recordingLabel.text = "Press to Record"
+        self.setState(newState: State.notRecording)
     }
 }
 
