05f293e21f036e38dc96d0e0d4abc28be6fae00c
everdimension <everdimension@gmail.com>
2019-01-27 13:43:53 +0300

    start/stop voice recording using AVAudioRecorder

--- pitch-hello/RecordSoundsViewController.swift
+++ pitch-hello/RecordSoundsViewController.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import AVFoundation
 
 class RecordSoundsViewController: UIViewController {
 
     @IBOutlet weak var recordingLabel: UILabel!
     @IBOutlet weak var recordButton: UIButton!
     @IBOutlet weak var stopRecordingButton: UIButton!
+    
+    var audioRecorder: AVAudioRecorder!
     
     enum State {
         case recording
@@ -51,10 +54,34 @@ class RecordSoundsViewController: UIViewController {
 
     @IBAction func recordAction(_ sender: Any) {
         self.setState(newState: State.recording)
+        
+        // Start recording
+        let dirPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
+        let recordingName = "recordedVoice.wav"
+        let pathArray = [dirPath, recordingName]
+        let filePath = URL(string: pathArray.joined(separator: "/"))
+        print(filePath)
+        
+        let session = AVAudioSession.sharedInstance()
+        try! session.setCategory(
+            AVAudioSession.Category.playAndRecord,
+            mode: AVAudioSession.Mode.default,
+            options: AVAudioSession.CategoryOptions.defaultToSpeaker
+        )
+        
+        try! audioRecorder = AVAudioRecorder(url: filePath!, settings: [:])
+        audioRecorder.isMeteringEnabled = true
+        audioRecorder.prepareToRecord()
+        audioRecorder.record()
     }
     
     @IBAction func stopRecordAction(_ sender: Any) {
         self.setState(newState: State.notRecording)
+        
+        // Stop recording
+        audioRecorder.stop()
+        let audioSession = AVAudioSession.sharedInstance()
+        try! audioSession.setActive(false)
     }
 }
 
